e5b19ef673c487b794f30844823c720b4bcf4167
hanai natsumi <hanai@iret.co.jp>
2024-12-26 15:19:33 +0900

    add: redux追加

--- app/redux/page.tsx
+++ app/redux/page.tsx
@@ -1,10 +1,13 @@
-import type { Metadata } from "next";
+"use client";
 import { Counter } from "../components/counter/Counter";
+import { StoreProvider } from "../StoreProvider";
 
 export default function IndexPage() {
-  return <Counter />;
+  return 
+  <StoreProvider><Counter /></StoreProvider>;
 }
 
+/*
 export const metadata: Metadata = {
   title: "Redux Toolkit(Test)",
-};
+};*/
